13001b016f96481c37eb973734c7974609118452
Keeghan <keeghan@magicaceconsulting.com>
2018-03-07 10:25:36 +0100

    SetBackground Function
    added the setbackground function, as well as a default Stoke & Fill color.

--- canvas-tools.js
+++ canvas-tools.js
@@ -1,3 +1,13 @@
+// Get HTML Canvas elements, and turn them into our own Canvas Objects
+function GetCanvas(id) {
+	return new Canvas(id);
+}
+
+function CreateCanvas(id,w,h,p) {
+	return new Canvas(id,w,h,p);
+}
+
+// Canvas Object Construct
 function Canvas(id,w,h,p){
 	if(arguments.length > 1){
 		let c = document.createElement("canvas");
@@ -14,12 +24,18 @@ function Canvas(id,w,h,p){
 	}
 	this.canvas = document.getElementById(id);
 	this.ctx    = this.canvas.getContext("2d");
+
+	// Black stroke and fill by default
+	this.defaultCol = "#000"
+	this.ctx.strokeStyle = this.defaultCol;
+	this.ctx.fillStyle = this.defaultCol;
 }
 
-function GetCanvas(id) {
-	return new Canvas(id);
-}
+// Canvas Property Functions
+Canvas.prototype.SetBackground = function(col) {
+	this.ctx.fillStyle = col;
+	this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
 
-function CreateCanvas(id,w,h,p) {
-	return new Canvas(id,w,h,p);
+	// Reset the default fill style for future drawing
+	this.ctx.fillStyle = this.defaultCol;
 }
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 canvas = GetCanvas("main-canvas");
-window.console.log(canvas);
+// window.console.log(canvas);
 
-canvas2 = CreateCanvas("second",150,150);
-window.console.log(canvas2);
+// canvas2 = CreateCanvas("second",150,150,"area");
+// window.console.log(canvas2);
+
+canvas.SetBackground("#AAA");
